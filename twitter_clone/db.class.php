e2cf0c2cd9b335b4d17236da93f71705aab4b14a
Claudio Monegatto <cmonegatto@gmail.com>
2019-11-27 17:21:24 -0300

    Atualização da aplicação Twitter

---
+++ twitter_clone/db.class.php
@@ -0,0 +1,30 @@
+<?php
+
+	class db {
+		 private $host = 'localhost';
+		 private $usuario = 'root';
+		 private $senha = '';
+		 private $database = 'twitter_clone';
+
+
+		 public function conecta_mysql(){
+
+		 	// criar a conexão
+		 	$con = mysqli_connect($this->host, $this->usuario, $this->senha, $this->database);
+
+		 	// ajustar o charset de comunicação entre a alicação e o DB
+		 	mysqli_set_charset($con, 'utf8');
+
+		 	// verificar se houve erro de conexão
+		 	if(mysqli_connect_errno()){
+		 		echo 'Erro ao tentar se conectar com o DB MySQL: ' . mysqli_connect_errno();		
+		 	}
+
+		 	return $con;
+
+		 }
+	}
+
+
+?>
+
